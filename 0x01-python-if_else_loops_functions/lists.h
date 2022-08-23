12c82ea75aecc2d0838be617972839fb6e9f7da0
IbukunAdejumo <ibukunadejumo@yahoomail.com>
2022-08-23 14:44:18 -0700

    Positive anything is better than negative nothing

---
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -0,0 +1,10 @@
+#ifndef LISTS_H
+
+def uppercase(str):
+def print_last_digit(number):
+def add(a, b):
+def pow(a, b):
+def fizzbuzz():
+listint_t *insert_node(listint_t **head, int number);
+
+#endif
