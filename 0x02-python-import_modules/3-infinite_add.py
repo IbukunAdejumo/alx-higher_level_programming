efa6d91945afa9565df32df8da90682c749de762
IbukunAdejumo <ibukunadejumo@yahoomail.com>
2022-08-29 12:23:35 -0700

    Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-from sys import argv
+import sys
 if __name__ == "__main__":
-    if len(argv) == 1:
-        print('{:d}'.format(0))
-    elif len(argv) == 2:
-        print('{:d}'.format(int(argv[1])))
-    elif len(argv) > 2:
-        sum = 0
-        for i in argv[1:]:
-            index = argv.index(i)
-            sum += int(argv[index])
-            print('{:d}'.format(sum))
+    n = len(sys.argv)
+    sum = 0
+    for i in range(1, n):
+        sum += int(sys.argv[i])
+        print(sum)
