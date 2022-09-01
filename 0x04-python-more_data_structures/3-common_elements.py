35acae4202889b4534a02a09973396f1dca385cd
IbukunAdejumo <ibukunadejumo@yahoomail.com>
2022-09-01 06:42:58 -0700

    Present in both

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    set_1 = set(1)
-    set_2 = set(2)
-
-    if (set_1 & set_2):
-        print(set_1 & set_2):
-            else:
-                print("No common elements")
+    return(set_1.intersection(set_2))
