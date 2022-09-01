bfce7a8033ad4ec7a16aed3a886dddf3cb6c21af
IbukunAdejumo <ibukunadejumo@yahoomail.com>
2022-09-01 07:36:13 -0700

    Task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def multiply_by_2(my_dict):
-    tmp_dict = my_dict.copy()
-    for x in tmp_dict.keys():
-        tmp_dict[x] *= 2
-        return (tmp_dict)
+def multiply_by_2(a_dictionary):
+    new_dict = a_dictionary.copy()
+    for i in new_dict:
+        new_dict[i] = new_dict[i] * 2
+        return new_dict
