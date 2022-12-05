75abc6eb48e940c11b6981f9e5ef09384e8f7d1a
Ibukun Adejumo <105447901+IbukunAdejumo@users.noreply.github.com>
2022-12-05 16:51:10 +0100

    Select States

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,40 @@
 #!/usr/bin/python3
-"""
-Lists all states from the database hbtn_0e_0_usa.
-Usage: ./0-select_states.py <mysql username> \
-                            <mysql password> \
-                             <database name>
-"""
-import sys
+"""script that lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
+from sys import argv
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+
+def get_dbase():
+    """Takes arguments argv and list from database
+    Arguments:
+        argv[1]: mysql username
+        argv[2]: mysql password
+        argv[3]: database name
+    """
+    dbase = MySQLdb.connect(host="localhost",
+                            port=3306,
+                            user=argv[1],
+                            passwd=argv[2],
+                            db=argv[3],
+                            charset="utf8"
+                            )
+
+    # Getting a cursor
+    dbase_cur = dbase.cursor()
+
+    # Esecuting dbase queries
+    dbase_cur.execute("SELECT * FROM states ORDER BY id")
+
+    # Fetches all the rows of a query result
+    query_rows = dbase_cur.fetchall()
+
+    # Print result one by one
+    for rows in query_rows:
+        print(rows)
+
+    dbase_cur.close()
+    dbase.close()
+
+
+if __name__ == '__main__':
+    get_dbase()
